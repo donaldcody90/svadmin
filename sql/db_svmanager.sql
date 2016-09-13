fd79d587d39ba6e965772613f9f1b151ea9a7a1f
toanphp <tadangtoan@hotmail.com>
2016-09-13 09:03:04 +0700

    update sql

--- sql/db_svmanager.sql
+++ sql/db_svmanager.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2016 at 05:05 AM
+-- Generation Time: Sep 13, 2016 at 04:02 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -28,32 +28,50 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `balance` (
   `cid` int(11) NOT NULL,
-  `amount` int(11) NOT NULL
+  `amount` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `balance`
+--
+
+INSERT INTO `balance` (`cid`, `amount`) VALUES
+(1001, '29978.34'),
+(1002, '38055.27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `billing`
+-- Table structure for table `billing_history`
 --
 
-CREATE TABLE `billing` (
-  `id` int(11) NOT NULL,
-  `cid` int(11) NOT NULL,
-  `type` int(11) NOT NULL,
-  `payment` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `billing_history` (
+  `id` bigint(18) NOT NULL,
+  `cid` bigint(18) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `created_date` varchar(255) DEFAULT NULL,
+  `amount` decimal(18,2) DEFAULT NULL,
+  `balance` decimal(18,2) DEFAULT NULL,
+  `type` int(1) DEFAULT '0' COMMENT '0, Invoice, 1 Nap tien',
+  `ref_id` bigint(18) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `billing`
+-- Dumping data for table `billing_history`
 --
 
-INSERT INTO `billing` (`id`, `cid`, `type`, `payment`) VALUES
-(1, 1001, 1, 2000),
-(2, 1002, 1, 3000),
-(3, 1003, 1, 4000),
-(4, 1004, 1, 5000),
-(5, 1006, 1, 6000);
+INSERT INTO `billing_history` (`id`, `cid`, `description`, `created_date`, `amount`, `balance`, `type`, `ref_id`) VALUES
+(1, 1002, 'Invoice#123456', '2016-09-01', '12.00', '10.00', 0, 1),
+(2, 1002, '123456', '2016-09-09', '22.14', '38105.04', 1, 1),
+(3, 1002, 'Invoice#123456', '2016-09-09', '0.00', '38105.04', 0, 1),
+(4, 1002, 'Invoice#123456', '2016-09-09', '0.42', '38103.57', 0, 1),
+(5, 1002, 'Invoice#123456', '2016-09-12', '8.58', '38057.91', 0, 1),
+(6, 1001, 'Invoice#324596', '2016-09-12', '0.44', '29978.35', 0, 3),
+(7, 1002, 'Invoice#123456', '2016-09-12', '5.06', '38055.49', 0, 1),
+(9, 1001, 'Invoice#301278', '2016-09-12', '0.00', '29978.35', 0, 4),
+(10, 1002, 'Invoice#123789', '2016-09-12', '0.00', '38055.49', 0, 2),
+(12, 1001, 'Invoice#301278', '2016-09-13', '0.02', '29978.34', 0, 4),
+(13, 1002, 'Invoice#123789', '2016-09-13', '0.23', '38055.27', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -165,6 +183,30 @@ INSERT INTO `customers` (`id`, `fullname`, `username`, `password`, `email`) VALU
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` bigint(18) NOT NULL,
+  `cid` bigint(18) NOT NULL,
+  `invoiceid` varchar(255) DEFAULT NULL,
+  `month` int(18) DEFAULT NULL,
+  `year` int(18) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`id`, `cid`, `invoiceid`, `month`, `year`) VALUES
+(1, 1002, '123456', 8, 2016),
+(2, 1002, '123789', 9, 2016),
+(3, 1001, '324596', 8, 2016),
+(4, 1001, '301278', 9, 2016);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `message`
 --
 
@@ -221,12 +263,44 @@ INSERT INTO `message` (`mid`, `cid`, `uid`, `content`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `os`
+--
+
+CREATE TABLE `os` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL COMMENT '1: 32bit   2: 64bit'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `os`
+--
+
+INSERT INTO `os` (`id`, `name`, `version`, `type`) VALUES
+(1, 'CentOS', '7', 2),
+(2, 'CentOS', '6', 2),
+(3, 'CentOS', '5', 2),
+(4, 'CoreOS', 'Stable', 2),
+(5, 'Debian', '8', 2),
+(6, 'Debian', '7', 2),
+(7, 'Fedora', '24', 2),
+(8, 'FreeBSD', '10', 2),
+(9, 'Ubuntu', '16.04', 2),
+(10, 'Ubuntu', '14.04', 2),
+(11, 'Ubuntu', '12.04', 2),
+(12, 'Windows', '2012 R2', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `plans`
 --
 
 CREATE TABLE `plans` (
   `id` bigint(18) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `os` varchar(255) DEFAULT NULL,
   `price` float(18,0) DEFAULT NULL,
   `cpu_core` bigint(18) DEFAULT NULL,
   `disk_space` bigint(18) DEFAULT NULL,
@@ -239,14 +313,42 @@ CREATE TABLE `plans` (
 -- Dumping data for table `plans`
 --
 
-INSERT INTO `plans` (`id`, `name`, `price`, `cpu_core`, `disk_space`, `ram`, `bandwidth`, `status`) VALUES
-(2, '15 GB SSD', 5, 1, 15, 768, 1000, 1),
-(3, '20 GB SSD', 10, 1, 20, 1024, 2000, 1),
-(4, '45 GB SSD', 20, 2, 45, 2048, 3000, 1),
-(5, '90 GB SSD', 40, 4, 90, 4096, 4000, 1),
-(6, '150 GB SSD', 80, 6, 150, 8192, 5000, 1),
-(7, '300 GB SSD', 160, 8, 300, 16384, 6000, 1),
-(8, '600 GB SSD', 320, 16, 600, 32768, 10000, 1);
+INSERT INTO `plans` (`id`, `name`, `os`, `price`, `cpu_core`, `disk_space`, `ram`, `bandwidth`, `status`) VALUES
+(2, '15 GB SSD', 'CentOS', 5, 1, 15, 768, 1000, 1),
+(3, '20 GB SSD', NULL, 10, 1, 20, 1024, 2000, 1),
+(4, '45 GB SSD', NULL, 20, 2, 45, 2048, 3000, 1),
+(5, '90 GB SSD', NULL, 40, 4, 90, 4096, 4000, 1),
+(6, '150 GB SSD', NULL, 80, 6, 150, 8192, 5000, 1),
+(7, '300 GB SSD', NULL, 160, 8, 300, 16384, 6000, 1),
+(8, '600 GB SSD', NULL, 320, 16, 600, 32768, 10000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recharge`
+--
+
+CREATE TABLE `recharge` (
+  `id` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '1:Paypal 2:Credit 3:Banktransfer',
+  `amount` decimal(18,2) NOT NULL,
+  `transaction_id` int(18) DEFAULT NULL,
+  `status` int(18) NOT NULL COMMENT '0:uncompleted   1:completed'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `recharge`
+--
+
+INSERT INTO `recharge` (`id`, `cid`, `type`, `amount`, `transaction_id`, `status`) VALUES
+(1, 1001, 1, '2000.00', NULL, 1),
+(2, 1002, 1, '3000.00', NULL, 1),
+(3, 1003, 1, '4000.00', NULL, 1),
+(4, 1004, 1, '5000.00', NULL, 1),
+(5, 1006, 1, '6000.00', NULL, 1),
+(7, 1001, 1, '21.00', NULL, 1),
+(8, 1002, 1, '194.00', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -275,7 +377,8 @@ INSERT INTO `servers` (`id`, `label`, `description`, `ip`, `svkey`, `svpass`, `l
 (28, 'Paris', '', '78.56.12.34', 'oislkdafjoisfslkjfls', 'alkdfjalsdfkvnasdlsfkdoafkl', 'France'),
 (29, 'London', '', '74.56.125.89', 'asdklvnadlknldnvkdknlkn', 'laskdfjoienvhsldkjoiasfdnoj', 'United Kingdom'),
 (30, 'Los Angeles', '', '78.32.16.96', 'alsdkfjasfknasvlskdjfoaiewofv', 'alkdjfaoiejfnoilksdjojfowijdf', 'United States'),
-(31, 'Sydney', '', '78.23.45.25', 'asldkfjsaofivnosldfolenfvoia', 'lakdfjoweinvoilkdsfieoidfkao', 'Australia');
+(31, 'Sydney', '', '78.23.45.25', 'asldkfjsaofivnosldfolenfvoia', 'lakdfjoweinvoilkdsfieoidfkao', 'Australia'),
+(32, 'Ha Noi', 'server test', '188.166.221.247', '6jktgaffibb7prf37iotrtqsn1dgmfz5', 'sygf0v1mf1meotn1qyt9uqzw3hyhft3e', 'VietNam');
 
 -- --------------------------------------------------------
 
@@ -347,23 +450,52 @@ CREATE TABLE `vps` (
   `vps_ip` varchar(255) DEFAULT NULL,
   `rootpass` varchar(255) DEFAULT NULL,
   `create_date` varchar(255) DEFAULT NULL,
-  `space` int(8) DEFAULT NULL,
-  `ram` int(8) DEFAULT NULL
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `vps`
 --
 
-INSERT INTO `vps` (`id`, `cid`, `pid`, `svid`, `vps_label`, `vps_ip`, `rootpass`, `create_date`, `space`, `ram`) VALUES
-(1, 1001, 2, 30, 'vps 1', '1.3.2.4', 'qwertyhgfd', '2016-6-12', 5, 1024),
-(2, 1002, 3, 26, 'vps 2', '1.2.3.7', 'xcvbnfesdg', '2016-8-2', 6, 1024),
-(3, 1002, 5, 27, 'vps 3', '8.2.0.6', 'ikolghncse', '2016-8-3', 7, 2048),
-(4, 1002, 4, 28, 'vps 4', '6.1.4.3', 'qwvbnhgdfe', '2016-8-5', 9, 2048),
-(5, 1001, 2, 29, 'vps 5', '8.4.6.1', 'dcfvgrwkln', '2016-8-9', 2, 4096),
-(6, 1002, 6, 25, 'DHQC', '1.1.1.1', 'ykhtgbfddpab165xcplvmrqfob68vu', '2016-08-25 03:46:54', NULL, NULL),
-(7, 1002, 2, 25, 'pppppppppppppppp', '1.1.1.1', 'yk73sfcyqzraz3fnb3t4vuvcfn4d7f', '2016-08-25 05:17:27', NULL, NULL),
-(8, 1002, 2, 25, 'toantest', '4.4.4.4', 'mybdj7hpsaa8sbwskw1rj4yzpjdsia', '2016-08-26 18:13:20', NULL, NULL);
+INSERT INTO `vps` (`id`, `cid`, `pid`, `svid`, `vps_label`, `vps_ip`, `rootpass`, `create_date`, `status`) VALUES
+(1, 1001, 2, 30, 'vps 1', '1.3.2.4', 'qwertyhgfd', '2016-6-12', 1),
+(2, 1002, 3, 26, 'vps 2', '1.2.3.7', 'xcvbnfesdg', '2016-8-2', 1),
+(3, 1002, 5, 27, 'vps 3', '8.2.0.6', 'ikolghncse', '2016-8-3', 1),
+(4, 1002, 4, 28, 'vps 4', '6.1.4.3', 'qwvbnhgdfe', '2016-8-5', 1),
+(5, 1001, 2, 29, 'vps 5', '8.4.6.1', 'dcfvgrwkln', '2016-8-9', 1),
+(6, 1002, 6, 25, 'DHQC', '1.1.1.1', 'ykhtgbfddpab165xcplvmrqfob68vu', '2016-08-25 03:46:54', 0),
+(7, 1002, 2, 25, 'pppppppppppppppp', '1.1.1.1', 'yk73sfcyqzraz3fnb3t4vuvcfn4d7f', '2016-08-25 05:17:27', 0),
+(8, 1002, 2, 25, 'toantest', '4.4.4.4', 'mybdj7hpsaa8sbwskw1rj4yzpjdsia', '2016-08-26 18:13:20', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vps_lifetime`
+--
+
+CREATE TABLE `vps_lifetime` (
+  `cid` bigint(18) NOT NULL,
+  `vps_id` bigint(18) NOT NULL,
+  `month` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `start_date` varchar(255) DEFAULT NULL,
+  `end_date` varchar(255) DEFAULT NULL,
+  `amount` decimal(18,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vps_lifetime`
+--
+
+INSERT INTO `vps_lifetime` (`cid`, `vps_id`, `month`, `year`, `start_date`, `end_date`, `amount`) VALUES
+(1001, 1, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1001, 5, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 2, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 3, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 4, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 6, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 7, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00'),
+(1002, 8, 9, 2016, '2016-09-13 04:01:06', '2016-09-13 04:01:06', '0.00');
 
 --
 -- Indexes for dumped tables
@@ -376,9 +508,9 @@ ALTER TABLE `balance`
   ADD PRIMARY KEY (`cid`);
 
 --
--- Indexes for table `billing`
+-- Indexes for table `billing_history`
 --
-ALTER TABLE `billing`
+ALTER TABLE `billing_history`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -427,15 +559,33 @@ ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
   ADD PRIMARY KEY (`mid`);
 
 --
+-- Indexes for table `os`
+--
+ALTER TABLE `os`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `plans`
 --
 ALTER TABLE `plans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recharge`
+--
+ALTER TABLE `recharge`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -463,14 +613,20 @@ ALTER TABLE `vps`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `vps_lifetime`
+--
+ALTER TABLE `vps_lifetime`
+  ADD PRIMARY KEY (`cid`,`vps_id`,`month`,`year`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `billing`
+-- AUTO_INCREMENT for table `billing_history`
 --
-ALTER TABLE `billing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `billing_history`
+  MODIFY `id` bigint(18) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `categories`
 --
@@ -492,20 +648,35 @@ ALTER TABLE `crypto_payments`
 ALTER TABLE `customers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
 --
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` bigint(18) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
   MODIFY `mid` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT for table `os`
+--
+ALTER TABLE `os`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `plans`
 --
 ALTER TABLE `plans`
   MODIFY `id` bigint(18) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT for table `recharge`
+--
+ALTER TABLE `recharge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `servers`
 --
 ALTER TABLE `servers`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `settings`
 --
